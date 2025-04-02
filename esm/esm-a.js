2ce637c0faec5130fa6b4b60ce0a2b137df22490
guesung <gueit214@naver.com>
2025-04-02 17:02:32 +0900

    feat: ESM - 키워드 없이 변수 할당

--- esm/esm-a.js
+++ esm/esm-a.js
@@ -1,3 +1,11 @@
 export const name = "박규성"; // ESM 환경에서는 import/export를 모듈 번들러 없이 사용할 수 있다.
 
 console.log(this); // ESM 환경에서 전역의 this는 undefined이다.
+
+try {
+  a = 5; // ESM환경은 기본적으로 엄격 모드이기에 var/let/const 키워드 없이 변수에 값을 할당하면 에러가 발생한다.
+
+  console.log(a);
+} catch (error) {
+  console.log(error); // ReferenceError: a is not defined
+}
